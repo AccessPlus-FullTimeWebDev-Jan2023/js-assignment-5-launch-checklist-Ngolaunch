80065ebc840d11d50db7504703ce084c2e0aa01d
ngohile <ngohile.yakubu@gmail.com>
2023-03-03 15:24:49 -0600

    adddest commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse= myFetch();
+
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    let selPlanet = pickPlanet(listedPlanets);
+    addDestinationInfo(document, selPlanet.name , selPlanet.diameter, selPlanet.star, selPlanet.distance, selPlanet.moons, selPlanet.imageUrl)
+    
+})
    
 });
