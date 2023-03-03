cbdb669860c9e89e8f94f3e181b7665796d60bad
ngohile <ngohile.yakubu@gmail.com>
2023-03-03 15:27:44 -0600

    add planets

--- scriptHelper.js
+++ scriptHelper.js
@@ -57,7 +57,6 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
         return response.json();
         });
