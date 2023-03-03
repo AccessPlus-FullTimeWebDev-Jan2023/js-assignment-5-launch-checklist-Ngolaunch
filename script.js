f1ccbdc6cbe5a13c741beb1da58e2daf0f05158c
ngohile <ngohile.yakubu@gmail.com>
2023-03-03 16:24:29 -0600

    test test

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     let selPlanet = pickPlanet(listedPlanets);
-    addDestinationInfo(document, selPlanet.name , selPlanet.diameter, selPlanet.star, selPlanet.distance, selPlanet.moons, selPlanet.imageUrl)
+    addDestinationInfo(document, selPlanet.name , selPlanet.diameter, selPlanet.star, selPlanet.distance, selPlanet.moons, selPlanet.imageUrl);
     
 })
    
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,18 +2,19 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    document.getElementById("missionTarget").innerHTML = 
    // Here is the HTML formatting for our mission target div.
-   /*
+   `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name:${name} </li>
+                    <li>Diameter:${diameter} </li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth:${distance} </li>
+                    <li>Number of Moons:${moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`
+   
 }
 //https://handlers.education.launchcode.org/static/planets.json
 
