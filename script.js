6347d8e0df159c22dd84a92f9745950299318b71
ngohile <ngohile.yakubu@gmail.com>
2023-03-03 20:57:20 -0600

    we on track

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet } = require("./scriptHelper");
+// const { formSubmission } = require("./scriptHelper");
+
+//const { myFetch, pickPlanet } = require("./scriptHelper");
+
+
 
 window.addEventListener("load", function() {
+
+    let launchform = document.getElementById("launchForm")
+
+    launchform.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        
+    let doc = document.getElementById("faultyItems")
+    let pilot = document.querySelector("input[name=pilotName]").value 
+    let copilot = document.querySelector("input[name=copilotName]").value 
+    let fuelLv = parseInt(document.querySelector("input[name=fuelLevel]").value) 
+    let cargo = parseInt(document.querySelector("input[name=cargoMass]").value)
+
+        formSubmission(document,doc,pilot,copilot,fuelLv,cargo);
+    })
+    
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -15,7 +35,7 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     let selPlanet = pickPlanet(listedPlanets);
-    addDestinationInfo(document, selPlanet.name , selPlanet.diameter, selPlanet.star, selPlanet.distance, selPlanet.moons, selPlanet.imageUrl);
+    addDestinationInfo(document, selPlanet.name , selPlanet.diameter, selPlanet.star, selPlanet.distance, selPlanet.moons, selPlanet.image);
     
 })
    
--- scriptHelper.js
+++ scriptHelper.js
@@ -29,28 +29,35 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   document.getElementById("pilotStatus").value = `Pilot ${pilot} is ready for launch`
-   document.getElementById("copilotStatus").value = `copilot ${copilot} is ready for launch`
-   
+   document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`
+   document.getElementById("copilotStatus").innerHTML = `copilot ${copilot} is ready for launch`
+   document.getElementById("cargoStatus").innerHTML ="Cargo mass low enough for launch"   
+   document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch"
+
+   //checking fuel level high enough
    if(fuelLevel < 10000){
     document.getElementById("faultyItems").style.visibility= "visible"
-    document.getElementById("fuelStatus").value = "Fuel level too low for launch"
-    document.getElementById("launchStatus").value = "Shuttle not ready for launch"
+    document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch"
+    document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
     document.getElementById("launchStatus").style.color= "red"
     }
-
+    
+    //cargolevel low enough
     if(cargoLevel > 10000){
      document.getElementById("faultyItems").style.visibility= "visible"
-     document.getElementById("cargoStatus").value ="Cargo mass too heavy for launch"   
-     document.getElementById("launchStatus").value = "Shuttle not ready for launch"   
+     document.getElementById("cargoStatus").innerHTML ="Cargo mass too heavy for launch"   
+     document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"   
      document.getElementById("launchStatus").style.color= "red"   
     }
 
-    else {
-    document.getElementById("launchStatus").value= "Shuttle is Ready for Launch"
+    //both conditions are met
+    if(fuelLevel >= 10000 && cargoLevel <= 10000){
+    document.getElementById("launchStatus").innerHTML= "Shuttle is Ready for Launch"
     document.getElementById("launchStatus").style.color= "green"
+    document.getElementById("faultyItems").style.visibility= "visible"
     
-    }
+    };
+
 
 
 }
